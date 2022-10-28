55599d0587fb6cad4e10ce6f4e14db24b15ac40d
Tarek Kariish <96070972+tarekkariish@users.noreply.github.com>
2022-10-28 15:57:17 +0200

    Web is done.
    needs to be resonsive only

--- Portfolio/home/myscript.js
+++ Portfolio/home/myscript.js
@@ -13,3 +13,23 @@ function opentab(tab){
 
     document.getElementById(tab).classList.add("active-tab");
 }
+
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwPyA7WQM4XrYUs4LHVKWIZUsFNWSFOhxnHQrv0gBLIshCWYT_53cuopeW7JKNDFUaJQg/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Thank you! We have recived your message"
+        setTimeout(function(){
+            msg.innerHTML=""
+        }, 6000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
